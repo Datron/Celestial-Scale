3600843501c66cdfdc05e61d01cb6b8fd419b2c4
Kartik <kartik@kartik.com>
2018-04-28 16:09:56 +0530

    update

--- main.cpp
+++ main.cpp
@@ -11,24 +11,25 @@ void display(){
     glMatrixMode(GL_MODELVIEW);
     universe_init();
     glLoadIdentity();
+    glOrtho(-1000,1000,-1000,1000,-1000,1000);
     glColor3f(1.0,0.0,0.0);
-    glRotatef(0.0, 1, 0, 0);
-    glRotatef(0.0, 0, 1, 0);
-    glRotatef(3.0, 0, 0, 1);
-    glTranslatef(0, 0, -1);
-    glutSolidSphere (0.5, 60, 60);
+    glTranslatef(-500,0,-1);
+    glutSolidSphere(200, 60, 60);
+    glTranslatef(500,0,-200);
+    glColor3f(0.0,0.6,0.7);
+    glutSolidSphere(200, 60, 60);
     glutSwapBuffers();
     glFlush();
 }
-// void reshape(int x, int y)
-// {
-//     if (y == 0 || x == 0) return;   
-//     glMatrixMode(GL_PROJECTION);  
-//     glLoadIdentity(); 
-//     gluPerspective(39.0,(GLdouble)x/(GLdouble)y,0.6,21.0);
-//     glMatrixMode(GL_MODELVIEW);
-//     glViewport(0,0,x,y);  //Use the whole window for rendering
-// }
+void reshape(int x, int y)
+{
+    if (y == 0 || x == 0) return;   
+    glMatrixMode(GL_PROJECTION);  
+    glLoadIdentity(); 
+    gluPerspective(39.0,(GLdouble)x/(GLdouble)y,0.6,21.0);
+    glMatrixMode(GL_MODELVIEW);
+    glViewport(0,0,x,y);  //Use the whole window for rendering
+}
 int main(int argc,char** argv){
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
